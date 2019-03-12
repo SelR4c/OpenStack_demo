33559da32fd23aefc24c6945df09a8e359aad061
devStack01 <cprevot@fortinet.com>
2019-03-12 17:03:39 +0100

    [ANSIBLE] script get IP FGT from openstack output

--- config/FGT/get_ip.sh
+++ config/FGT/get_ip.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
+# name of the stack
 STACK='test'
+
+# name of outputs, specified in the OUTPUTS section of heat template
+FGT_WAN='FGT_WAN'
+FGT_LAN='FGT_LAN'
 FGT_OUTPUT='FGTs'
 WS_OUTPUT='Web_Servers'
 
-IP=$(openstack stack output show -f json $STACK $FGT_OUTPUT | jq '.output_value | .[][1]' | sed -e "s/^\$*/\t- /g")
-echo "---\nIP:\n$IP" > fortigate_IP.yaml
-IP=$(openstack stack output show -f json $STACK $WS_OUTPUT | jq '.output_value | .[][1]' | sed -e "s/^\$*/\t- /g")
-echo "---\nIP:\n$IP" > web_servers_IP.yaml
+FGT_WAN_IP=$(openstack stack output show -f json $STACK $FGT_WAN | \
+  jq '.output_value | .[1]' | sed -e "s/^\$*//g")
+echo "---\nIP:" > fortigate_IP.yaml
+echo "  WAN: $FGT_WAN_IP" >> fortigate_IP.yaml
+
+FGT_LAN_IP=$(openstack stack output show -f json $STACK $FGT_LAN | \
+  jq '.output_value | .[1]' | sed -e "s/^\$*//g")
+echo "  LAN: $FGT_LAN_IP" >> fortigate_IP.yaml
+
+FGTs_IP=$(openstack stack output show -f json $STACK $FGT_OUTPUT | \
+  jq '.output_value | .[][1]' | sed -e "s/^\$*/    - /g")
+echo "  FGTs_IP:\n$FGTs_IP" >> fortigate_IP.yaml
