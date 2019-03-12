4143ebd4e820ded09f3fe270712afd21d7d3ad6e
devStack01 <cprevot@fortinet.com>
2019-03-12 14:12:41 +0100

    [ANSIBLE] Infra openstack terminée, début des config ansible

--- config/FGT/get_ip.sh
+++ config/FGT/get_ip.sh
@@ -4,7 +4,6 @@ STACK='test'
 FGT_OUTPUT='FGTs'
 WS_OUTPUT='Web_Servers'
 
-cd config
 IP=$(openstack stack output show -f json $STACK $FGT_OUTPUT | jq '.output_value | .[][1]' | sed -e "s/^\$*/\t- /g")
 echo "---\nIP:\n$IP" > fortigate_IP.yaml
 IP=$(openstack stack output show -f json $STACK $WS_OUTPUT | jq '.output_value | .[][1]' | sed -e "s/^\$*/\t- /g")
