347b20acf04c95d2c461414a6d10c31828da7269
devStack01 <cprevot@fortinet.com>
2019-03-13 17:02:06 +0100

    [ANSIBLE] finish ansible, bug loop set cluster HA in FGT_HA.yaml

--- config/FGT/get_ip.sh
+++ config/FGT/get_ip.sh
@@ -8,6 +8,10 @@ FGT_WAN='FGT_WAN'
 FGT_LAN='FGT_LAN'
 FGT_OUTPUT='FGTs'
 WS_OUTPUT='Web_Servers'
+WAN_CIRD='WAN_CIDR'
+LAN_CIRD='LAN_CIDR'
+NET01_CIRD='NET01_CIDR'
+HA_CIRD='HA_CIDR'
 
 FGT_WAN_IP=$(openstack stack output show -f json $STACK $FGT_WAN | \
   jq '.output_value | .[1]' | sed -e "s/^\$*//g")
@@ -20,4 +24,21 @@ echo "  LAN: $FGT_LAN_IP" >> fortigate_IP.yaml
 
 FGTs_IP=$(openstack stack output show -f json $STACK $FGT_OUTPUT | \
   jq '.output_value | .[][1]' | sed -e "s/^\$*/    - /g")
-echo "  FGTs_IP:\n$FGTs_IP" >> fortigate_IP.yaml
+echo "  FGTs_HA:\n$FGTs_IP" >> fortigate_IP.yaml
+
+echo "CIDR:" >> fortigate_IP.yaml
+CIDR=$(openstack stack output show -f json $STACK $WAN_CIRD | \
+  jq '.output_value' | sed -e "s/^\$*//g")
+echo "  WAN: $CIDR" >> fortigate_IP.yaml
+
+CIDR=$(openstack stack output show -f json $STACK $LAN_CIRD | \
+  jq '.output_value' | sed -e "s/^\$*//g")
+echo "  LAN: $CIDR" >> fortigate_IP.yaml
+
+CIDR=$(openstack stack output show -f json $STACK $NET01_CIRD | \
+  jq '.output_value' | sed -e "s/^\$*//g")
+echo "  NET01: $CIDR" >> fortigate_IP.yaml
+
+CIDR=$(openstack stack output show -f json $STACK $HA_CIRD | \
+  jq '.output_value' | sed -e "s/^\$*//g")
+echo "  HA: $CIDR" >> fortigate_IP.yaml
