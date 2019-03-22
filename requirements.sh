475d576c00a7f34d78169665c03103df52259617
Charles Prevot <cprevot@fortinet-us.com>
2019-03-22 17:38:53 +0100

    requirements.sh update

--- requirements.sh
+++ requirements.sh
@@ -2,9 +2,7 @@
 
 set -ex
 
-DEMO_FOLDER="$(pwd)"
-
-cd $HOME
+cd ..
 # latest version for ubuntu/debian
 sudo apt install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
@@ -16,12 +14,18 @@ sudo apt-get install git jq ansible
 # 40ansible
 git clone https://github.com/fortinet-solutions-cse/40ansible.git --depth 1
 sudo pip install fortiosapi
-# needed to run locally
-echo "export ANSIBLE_LIBRARY=$(pwd)/40ansible/library" >> ~/.bash_profile
 
 git clone https://github.com/openstack-dev/devstack.git
-cp "$DEMO_FOLDER/local.conf" ./devstack/local.conf
+cp OpenStack_demo/local.conf ./devstack/local.conf
+
+sudo ./devstack/tools/create-stack-user.sh
+
+# needed to run locally (with user stack)
+echo "export ANSIBLE_LIBRARY=$(pwd)/40ansible/library" >> /home/stack/.profile
+
 cd devstack
 git checkout stable/queens # Demo was build and tested with queens only
+
+./stack.sh
 
 exit 0
