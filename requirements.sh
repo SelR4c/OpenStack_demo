52fc8a0d00ced3e3fff4a4cca3b29539545ccd84
Charles Prevot <cprevot@fortinet-us.com>
2019-03-22 17:44:08 +0100

    requirements.sh update 2

--- requirements.sh
+++ requirements.sh
@@ -4,16 +4,12 @@ set -ex
 
 cd ..
 # latest version for ubuntu/debian
-sudo apt install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
 
 sudo apt-get update
 
-sudo apt-get install git jq ansible
-
-# 40ansible
-git clone https://github.com/fortinet-solutions-cse/40ansible.git --depth 1
-sudo pip install fortiosapi
+sudo apt-get install -y git jq ansible
 
 git clone https://github.com/openstack-dev/devstack.git
 cp OpenStack_demo/local.conf ./devstack/local.conf
@@ -27,5 +23,10 @@ cd devstack
 git checkout stable/queens # Demo was build and tested with queens only
 
 ./stack.sh
+
+cd ..
+# 40ansible
+git clone https://github.com/fortinet-solutions-cse/40ansible.git --depth 1
+sudo pip install fortiosapi
 
 exit 0
